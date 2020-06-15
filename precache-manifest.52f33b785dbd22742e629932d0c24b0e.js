3a54d4e56e779003c340123434a5c70f6a6c42dc
Arc <arky.happ@gmail.com>
2020-06-16 00:09:11 +0300

    Updates

---
+++ precache-manifest.52f33b785dbd22742e629932d0c24b0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca468eecd5b35213cd881369ac927a81",
+    "url": "/team-rolodex/index.html"
+  },
+  {
+    "revision": "0885b74d5f15e1051cbb",
+    "url": "/team-rolodex/static/css/main.93170c35.chunk.css"
+  },
+  {
+    "revision": "fc613ec4a263851b7ef1",
+    "url": "/team-rolodex/static/js/2.5206ad0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/team-rolodex/static/js/2.5206ad0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0885b74d5f15e1051cbb",
+    "url": "/team-rolodex/static/js/main.4e2dd15d.chunk.js"
+  },
+  {
+    "revision": "5d1a3fecdfbead6cf5f1",
+    "url": "/team-rolodex/static/js/runtime-main.de46c542.js"
+  },
+  {
+    "revision": "e7352946044da1eed39158194485f3b3",
+    "url": "/team-rolodex/static/media/bg.e7352946.jpg"
+  }
+]);
